972c498cb9ff2c2801d4b20de23c43f3bb26ec98
Daniel <dgualteros637@gmail.com>
2023-10-27 19:56:21 -0500

    actualizad

--- scholar/students/models.py
+++ scholar/students/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     email=models.CharField(max_length=100)
+    
     password=models.CharField(max_length=500)
+
+    dni=models.CharField(max_length=10, default='')
