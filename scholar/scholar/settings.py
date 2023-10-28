1cc65e6c3d46e394d4dddbc800a772ded7a1e7b1
Daniel <dgualteros637@gmail.com>
2023-10-27 19:34:30 -0500

    actualizado

--- scholar/scholar/settings.py
+++ scholar/scholar/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'students.apps.ConfigStudents',
+    'students.apps.StudentsConfig',
 ]
 
 MIDDLEWARE = [
